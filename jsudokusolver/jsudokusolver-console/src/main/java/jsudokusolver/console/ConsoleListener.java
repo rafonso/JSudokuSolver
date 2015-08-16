9d4d4273195b5222ef23de79b6f3ff44595af92e
Rafael Ubiratam Clemente Afonso <rucafonso@hotmail.com>
2015-08-16 18:10:35 -0300

    Added ConsoleListener to show the cnahges in Console.
    Added dependency from slf4j + Log4J

---
+++ jsudokusolver/jsudokusolver-console/src/main/java/jsudokusolver/console/ConsoleListener.java
@@ -0,0 +1,76 @@
+package jsudokusolver.console;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jsudokusolver.core.Cell;
+import jsudokusolver.core.CellStatus;
+import jsudokusolver.core.Puzzle;
+import jsudokusolver.core.PuzzleStatus;
+
+public class ConsoleListener implements PropertyChangeListener {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	public ConsoleListener() {
+	}
+
+	private void cellValueChanged(Cell cell, Optional<Integer> from, Optional<Integer> to) {
+		if (log.isTraceEnabled()) {
+			String msg;
+			if (from.isPresent() && to.isPresent()) {
+				msg = "Changing from " + from.get() + " to " + to.get();
+			} else if (!from.isPresent() && to.isPresent()) {
+				msg = "Filling with " + to.get();
+			} else if (from.isPresent() && !to.isPresent()) {
+				msg = "Removing " + from.get();
+			} else if (from.isPresent() && !to.isPresent()) {
+				msg = "Changing from " + from.get() + " to " + to.get();
+			} else {
+				msg = "Maintaining empty";
+			}
+
+			log.trace("Cell({},{}): {}", cell.getRow(), cell.getColumn(), msg);
+
+		}
+	}
+
+	private void cellStatusChanged(Cell cell, CellStatus from, CellStatus to) {
+		if (log.isTraceEnabled()) {
+			log.trace("Cell({},{}): changing status from {} to {}", cell.getRow(), cell.getColumn(), from, to);
+		}
+	}
+
+	private void puzzleStatusChanged(Puzzle source, PuzzleStatus from, PuzzleStatus to) {
+		if (log.isTraceEnabled()) {
+			log.trace("Puzzle   : changing status from {} to {}", from, to);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public void propertyChange(PropertyChangeEvent evt) {
+		switch (evt.getPropertyName()) {
+		case Cell.CELL_VALUE:
+			this.cellValueChanged((Cell) evt.getSource(), (Optional<Integer>) evt.getOldValue(),
+					(Optional<Integer>) evt.getNewValue());
+			break;
+		case Cell.CELL_STATUS:
+			this.cellStatusChanged((Cell) evt.getSource(), (CellStatus) evt.getOldValue(),
+					(CellStatus) evt.getNewValue());
+			break;
+		case Puzzle.PUZZLE_STATUS:
+			this.puzzleStatusChanged((Puzzle) evt.getSource(), (PuzzleStatus) evt.getOldValue(),
+					(PuzzleStatus) evt.getNewValue());
+			break;
+		// TODO SOlver Cycle
+		default:
+			throw new IllegalStateException("Irrregular change type: " + evt.getPropertyName() + ". Event: " + evt);
+		}
+
+	}
+
+}
--- jsudokusolver/jsudokusolver-console/src/main/java/jsudokusolver/console/SudokuSolver.java
+++ jsudokusolver/jsudokusolver-console/src/main/java/jsudokusolver/console/SudokuSolver.java
@@ -1,6 +1,14 @@
 package jsudokusolver.console;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import jsudokusolver.core.Cell;
+import jsudokusolver.core.CellFunctions;
+import jsudokusolver.core.CellStatus;
 import jsudokusolver.core.Puzzle;
+import jsudokusolver.core.PuzzlePositions;
 import jsudokusolver.core.Validator;
 
 public class SudokuSolver {
@@ -19,16 +27,38 @@ public class SudokuSolver {
 		}
 
 		Puzzle p = new Puzzle();
+		ConsoleListener listener = new ConsoleListener();
+		p.addPropertyChangeListener(listener);
 		for (int pos = 0; pos < digits.toCharArray().length; pos++) {
+			final Cell cell = p.getCell((pos / 9) + 1, (pos % 9) + 1);
+			cell.addPropertyChangeListener(listener);
+
 			int digit = digits.toCharArray()[pos] - '0';
 			if (digit > 0) {
-				int row = (pos / 9) + 1;
-				int col = (pos % 9) + 1;
-				p.getCell(row, col).setValue(digit);
+				cell.setValue(digit);
+				cell.setStatus(CellStatus.FILLED);
 			}
 		}
 
 		return p;
+	}
+
+	private static String formatPuzzle(Puzzle p) {
+		final Function<? super Integer, ? extends String> formatRow = row -> {
+			Integer[] values = p.getCellsStream().filter(PuzzlePositions.ROW.getPositionPredicate(row))
+					.map(CellFunctions.CELL_TO_VALUE_OR_0).collect(Collectors.toList()).toArray(new Integer[9]);
+			return String.format("|%d%d%d|%d%d%d|%d%d%d|%n", values[0], values[1], values[2], values[3], values[4],
+					values[5], values[6], values[7], values[8]);
+		};
+		final String rowSectorSeparator = "|---+---+---|\n";
+
+		String[] formatedRows = CellFunctions.rangeStream().map(formatRow).collect(Collectors.toList())
+				.toArray(new String[9]);
+
+		return rowSectorSeparator + formatedRows[0] + formatedRows[1] + formatedRows[2] //
+				+ rowSectorSeparator + formatedRows[3] + formatedRows[4] + formatedRows[5] //
+				+ rowSectorSeparator + formatedRows[6] + formatedRows[7] + formatedRows[8] //
+				+ rowSectorSeparator;
 	}
 
 	public static void main(String[] args) {
@@ -40,7 +70,7 @@ public class SudokuSolver {
 		Puzzle puzzle = commandToPuzzle(args[0]);
 		new Validator().validate(puzzle);
 
-		System.out.println(puzzle);
+		System.out.println(formatPuzzle(puzzle));
 	}
 
 }
